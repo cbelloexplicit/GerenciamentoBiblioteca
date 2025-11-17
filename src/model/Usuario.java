e06baf9d88dcf8bbdbe45a2fb4978b8c5c1e7b25
cbelloexplicit <97253486+cbelloexplicit@users.noreply.github.com>
2025-11-17 19:50:17 -0300

    Update Usuario.java

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,4 +1,17 @@
 package model;
 
 public class Usuario {
+
+    private String id_usuario;
+    private String nome;
+    private int matricula;
+    private String senha_hash;
+	private String tipo_usuario;
+
+    public Usuario(String id_usuario, String nome, int matricula, String senha_hash, String tipo_usuario){
+        this.id_usuario = id_usuario;
+        this.matricula = matricula;
+        this.senha_hash = senha_hash;
+        this.tipo_usuario = tipo_usuario;
+    }
 }
